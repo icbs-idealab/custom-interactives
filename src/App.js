2b34e8b7f30d1725748093be9ee3542ee0ea5ea3
Peter Atkinson <peterjatkinson@gmail.com>
2024-12-31 15:30:21 +0000

    Updated routing to use folders for modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ResizeWrapper from './ResizeWrapper';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import AITimeline from './apps/AITimeline.js';
 import AdvertisingTimeline from './apps/AdvertisingTimeline.js';
 import AverageCalculator from './apps/AverageCalculator.js';
 import BreakEven from './apps/BreakEven.js';
@@ -18,8 +16,35 @@ import MarketingDashboard from './apps/MarketingDashboard.js';
 import PositioningStatementCreator from './apps/PositioningStatementCreator.js';
 import PositioningStatementGlobal from './apps/PositioningStatementGlobal.js';
 import TestApp from './apps/TestApp.js';
-import TimModuleMap from './apps/TimModuleMap.js';
 import ToggleSwitchApp from './apps/ToggleSwitchApp.js';
+import SMO_TiM_AITimeline from './apps/SMO_TiM/AITimeline.js';
+import SMO_TiM_TimModuleMap from './apps/SMO_TiM/TimModuleMap.js';
+
+/**
+ * We define these constants so the JSX can reference them
+ * (and won't throw "no-undef" errors).
+ */
+const modules = {
+  "SMO_TiM": [
+    "AITimeline.js",
+    "TimModuleMap.js"
+  ]
+};
+const topLevelApps = [
+  "AdvertisingTimeline.js",
+  "AverageCalculator.js",
+  "BreakEven.js",
+  "CampaignIdeas.js",
+  "CounterApp.js",
+  "DoubleNumber.js",
+  "DragDropTimeline.js",
+  "FluidAI.js",
+  "MarketingDashboard.js",
+  "PositioningStatementCreator.js",
+  "PositioningStatementGlobal.js",
+  "TestApp.js",
+  "ToggleSwitchApp.js"
+];
 
 
 function DynamicTitle() {
@@ -27,21 +52,22 @@ function DynamicTitle() {
 
   useEffect(() => {
     const titles = {
-      '/aitimeline': 'A I Timeline',
-  '/advertisingtimeline': 'Advertising Timeline',
-  '/averagecalculator': 'Average Calculator',
-  '/breakeven': 'Break Even',
-  '/campaignideas': 'Campaign Ideas',
-  '/counterapp': 'Counter App',
-  '/doublenumber': 'Double Number',
-  '/dragdroptimeline': 'Drag Drop Timeline',
-  '/fluidai': 'Fluid A I',
-  '/marketingdashboard': 'Marketing Dashboard',
-  '/positioningstatementcreator': 'Positioning Statement Creator',
-  '/positioningstatementglobal': 'Positioning Statement Global',
-  '/testapp': 'Test App',
-  '/timmodulemap': 'Tim Module Map',
-  '/toggleswitchapp': 'Toggle Switch App'
+      '/advertisingtimeline': 'Advertising Timeline',
+      '/averagecalculator': 'Average Calculator',
+      '/breakeven': 'Break Even',
+      '/campaignideas': 'Campaign Ideas',
+      '/counterapp': 'Counter App',
+      '/doublenumber': 'Double Number',
+      '/dragdroptimeline': 'Drag Drop Timeline',
+      '/fluidai': 'Fluid A I',
+      '/marketingdashboard': 'Marketing Dashboard',
+      '/positioningstatementcreator': 'Positioning Statement Creator',
+      '/positioningstatementglobal': 'Positioning Statement Global',
+      '/testapp': 'Test App',
+      '/toggleswitchapp': 'Toggle Switch App',
+      '/smo_tim': 'SMO_TiM',
+      '/smo_tim/aitimeline': 'A I Timeline',
+      '/smo_tim/timmodulemap': 'Tim Module Map'
     };
     document.title = titles[location.pathname] || 'Interactive Learning Apps';
   }, [location]);
@@ -55,49 +81,278 @@ function App() {
     <Router>
       <DynamicTitle />
       <Routes>
+        {/* The homepage */}
         <Route
           path="/"
           element={
-            <div style={{ textAlign: 'center', padding: '2rem' }}>
-              <h1>Welcome to Interactive Learning Apps</h1>
-              <p>Select an app to get started:</p>
-              <nav style={{ marginTop: '1rem' }}>
-                <ul style={{ listStyle: 'none', padding: 0 }}>
-                  <li style={{ marginBottom: '1rem' }}><Link to="/aitimeline" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> A I Timeline</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/advertisingtimeline" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Advertising Timeline</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/averagecalculator" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Average Calculator</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/breakeven" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Break Even</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/campaignideas" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Campaign Ideas</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/counterapp" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Counter App</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/doublenumber" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Double Number</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/dragdroptimeline" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Drag Drop Timeline</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/fluidai" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Fluid A I</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/marketingdashboard" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Marketing Dashboard</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/positioningstatementcreator" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Positioning Statement Creator</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/positioningstatementglobal" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Positioning Statement Global</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/testapp" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Test App</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/timmodulemap" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Tim Module Map</Link></li>
-                <li style={{ marginBottom: '1rem' }}><Link to="/toggleswitchapp" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}> Toggle Switch App</Link></li>
-                </ul>
-              </nav>
-            </div>
+            
+<div style={{ textAlign: 'center', padding: '2rem' }}>
+  <h1>Welcome to Interactive Learning Apps</h1>
+  <nav style={{ marginTop: '2rem' }}>
+
+    {/* Only show Modules heading if there's at least one module */}
+    {Object.keys(modules).length > 0 && (
+      <>
+        <h2>Modules</h2>
+        <ul style={{ listStyle: 'none', padding: 0 }}>
+          
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/smo_tim" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        SMO_TiM
+      </Link>
+    </li>
+  
+        </ul>
+      </>
+    )}
+
+    {/* Only show Other Apps heading if there's at least one top-level app */}
+    {topLevelApps.length > 0 && (
+      <>
+        <h2>Other apps</h2>
+        <ul style={{ listStyle: 'none', padding: 0 }}>
+          
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/advertisingtimeline" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Advertising Timeline
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/averagecalculator" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Average Calculator
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/breakeven" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Break Even
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/campaignideas" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Campaign Ideas
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/counterapp" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Counter App
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/doublenumber" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Double Number
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/dragdroptimeline" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Drag Drop Timeline
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/fluidai" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Fluid A I
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/marketingdashboard" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Marketing Dashboard
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/positioningstatementcreator" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Positioning Statement Creator
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/positioningstatementglobal" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Positioning Statement Global
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/testapp" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Test App
+      </Link>
+    </li>
+  
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="/toggleswitchapp" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        Toggle Switch App
+      </Link>
+    </li>
+  
+        </ul>
+      </>
+    )}
+
+  </nav>
+</div>
+
           }
         />
-        <Route path="/aitimeline" element={<ResizeWrapper><AITimeline /></ResizeWrapper>} />
-        <Route path="/advertisingtimeline" element={<ResizeWrapper><AdvertisingTimeline /></ResizeWrapper>} />
-        <Route path="/averagecalculator" element={<ResizeWrapper><AverageCalculator /></ResizeWrapper>} />
-        <Route path="/breakeven" element={<ResizeWrapper><BreakEven /></ResizeWrapper>} />
-        <Route path="/campaignideas" element={<ResizeWrapper><CampaignIdeas /></ResizeWrapper>} />
-        <Route path="/counterapp" element={<ResizeWrapper><CounterApp /></ResizeWrapper>} />
-        <Route path="/doublenumber" element={<ResizeWrapper><DoubleNumber /></ResizeWrapper>} />
-        <Route path="/dragdroptimeline" element={<ResizeWrapper><DragDropTimeline /></ResizeWrapper>} />
-        <Route path="/fluidai" element={<ResizeWrapper><FluidAI /></ResizeWrapper>} />
-        <Route path="/marketingdashboard" element={<ResizeWrapper><MarketingDashboard /></ResizeWrapper>} />
-        <Route path="/positioningstatementcreator" element={<ResizeWrapper><PositioningStatementCreator /></ResizeWrapper>} />
-        <Route path="/positioningstatementglobal" element={<ResizeWrapper><PositioningStatementGlobal /></ResizeWrapper>} />
-        <Route path="/testapp" element={<ResizeWrapper><TestApp /></ResizeWrapper>} />
-        <Route path="/timmodulemap" element={<ResizeWrapper><TimModuleMap /></ResizeWrapper>} />
-        <Route path="/toggleswitchapp" element={<ResizeWrapper><ToggleSwitchApp /></ResizeWrapper>} />
+
+        {/* Top-level app routes */}
+        
+  <Route
+    path="/advertisingtimeline"
+    element={
+      <ResizeWrapper>
+        <AdvertisingTimeline />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/averagecalculator"
+    element={
+      <ResizeWrapper>
+        <AverageCalculator />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/breakeven"
+    element={
+      <ResizeWrapper>
+        <BreakEven />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/campaignideas"
+    element={
+      <ResizeWrapper>
+        <CampaignIdeas />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/counterapp"
+    element={
+      <ResizeWrapper>
+        <CounterApp />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/doublenumber"
+    element={
+      <ResizeWrapper>
+        <DoubleNumber />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/dragdroptimeline"
+    element={
+      <ResizeWrapper>
+        <DragDropTimeline />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/fluidai"
+    element={
+      <ResizeWrapper>
+        <FluidAI />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/marketingdashboard"
+    element={
+      <ResizeWrapper>
+        <MarketingDashboard />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/positioningstatementcreator"
+    element={
+      <ResizeWrapper>
+        <PositioningStatementCreator />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/positioningstatementglobal"
+    element={
+      <ResizeWrapper>
+        <PositioningStatementGlobal />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/testapp"
+    element={
+      <ResizeWrapper>
+        <TestApp />
+      </ResizeWrapper>
+    }
+  />
+  <Route
+    path="/toggleswitchapp"
+    element={
+      <ResizeWrapper>
+        <ToggleSwitchApp />
+      </ResizeWrapper>
+    }
+  />
+
+        {/* Module routes */}
+        
+    <Route
+      path="/smo_tim"
+      element={
+        <div style={{ textAlign: 'center', padding: '2rem' }}>
+          <h2>SMO_TiM module</h2>
+          <ul style={{ listStyle: 'none', padding: 0 }}>
+            
+                  <li style={{ marginBottom: '1rem' }}>
+                    <Link to="/smo_tim/aitimeline" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+                      A I Timeline
+                    </Link>
+                  </li>
+                
+                  <li style={{ marginBottom: '1rem' }}>
+                    <Link to="/smo_tim/timmodulemap" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+                      Tim Module Map
+                    </Link>
+                  </li>
+                
+          </ul>
+        </div>
+      }
+    />
+  
+
+      <Route
+        path="/smo_tim/aitimeline"
+        element={
+          <ResizeWrapper>
+            <SMO_TiM_AITimeline />
+          </ResizeWrapper>
+        }
+      />
+    
+
+      <Route
+        path="/smo_tim/timmodulemap"
+        element={
+          <ResizeWrapper>
+            <SMO_TiM_TimModuleMap />
+          </ResizeWrapper>
+        }
+      />
+    
       </Routes>
     </Router>
   );
--- updateAppJs.js
+++ updateAppJs.js
@@ -1,51 +1,127 @@
+/**
+ * update-app.js
+ *
+ * Run `node update-app.js` in your project root.
+ * It will generate/overwrite `src/App.js` with dynamically built routes
+ * for all top-level JS apps and any module subfolders under `src/apps`.
+ */
+
 const fs = require('fs');
 const path = require('path');
 
-// Paths to the apps directory and App.js
+// --------------------------
+// CONFIG: Adjust if needed
+// --------------------------
 const appsDir = path.join(__dirname, 'src', 'apps');
 const appJsPath = path.join(__dirname, 'src', 'App.js');
 
-// Read all .js files in the apps directory
-const files = fs.readdirSync(appsDir).filter(file => file.endsWith('.js'));
+// --------------------------
+// HELPER FUNCTIONS
+// --------------------------
+function stripExtension(filename) {
+  return filename.replace('.js', '');
+}
 
-// Generate imports for each app and the ResizeWrapper
-const imports = `import ResizeWrapper from './ResizeWrapper';\nimport { useEffect } from 'react';\nimport { useLocation } from 'react-router-dom';\n` +
-                files.map(file => `import ${path.basename(file, '.js')} from './apps/${file}';`).join('\n');
+/**
+ * For route paths, we just want lowercase versions
+ * of the filename or folder name (minus the .js).
+ */
+function toRouteName(str) {
+  return stripExtension(str).toLowerCase();
+}
 
-// Generate title map for DynamicTitle component
-const titleMap = files
-  .map(file => {
-    const routeName = path.basename(file, '.js').toLowerCase();
-    const title = path.basename(file, '.js').replace(/([A-Z])/g, ' $1').trim();
-    return `'/${routeName}': '${title}'`;
-  })
-  .join(',\n  ');
+/**
+ * Insert spaces before capital letters for a "nice" display.
+ * E.g., "MyApp.js" -> "My App", "CampaignIdeas.js" -> "Campaign Ideas".
+ */
+function toNiceTitle(str) {
+  const base = stripExtension(str);
+  return base.replace(/([A-Z])/g, ' $1').trim();
+}
 
-// Generate routes wrapped in ResizeWrapper
-const routes = files
-  .map(file => {
-    const routeName = path.basename(file, '.js').toLowerCase();
-    return `<Route path="/${routeName}" element={<ResizeWrapper><${path.basename(file, '.js')} /></ResizeWrapper>} />`;
-  })
-  .join('\n        ');
+// --------------------------
+// 1. DISCOVER APPS & MODULES
+// --------------------------
+const topLevelApps = [];
+const modules = {};
 
-// Generate links for each app
-const links = files
-  .map(file => {
-    const linkName = path.basename(file, '.js');
-    const routeName = linkName.toLowerCase();
-    return `<li style={{ marginBottom: '1rem' }}><Link to="/${routeName}" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>${linkName.replace(/([A-Z])/g, ' $1')}</Link></li>`;
-  })
-  .join('\n                ');
+const items = fs.readdirSync(appsDir, { withFileTypes: true });
+items.forEach(dirent => {
+  if (dirent.isDirectory()) {
+    const moduleName = dirent.name; // e.g. "ModuleA"
+    const subDir = path.join(appsDir, moduleName);
+    const filesInModule = fs
+      .readdirSync(subDir)
+      .filter(file => file.endsWith('.js'));
 
-// Template for the DynamicTitle component
+    if (filesInModule.length > 0) {
+      modules[moduleName] = filesInModule; // e.g. { "ModuleA": ["SomeApp.js", "Another.js"] }
+    }
+  } else if (dirent.isFile() && dirent.name.endsWith('.js')) {
+    topLevelApps.push(dirent.name); // e.g. "MyApp.js"
+  }
+});
+
+// --------------------------
+// 2. BUILD IMPORT LINES
+// --------------------------
+const importLines = [];
+importLines.push(`import React from 'react';`);
+importLines.push(`import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';`);
+importLines.push(`import ResizeWrapper from './ResizeWrapper';`);
+importLines.push(`import { useEffect } from 'react';`);
+importLines.push(`import { useLocation } from 'react-router-dom';`);
+
+// For top-level apps
+topLevelApps.forEach(filename => {
+  const baseName = stripExtension(filename);
+  importLines.push(`import ${baseName} from './apps/${filename}';`);
+});
+
+// For module apps
+const moduleImports = {};
+Object.keys(modules).forEach(moduleName => {
+  modules[moduleName].forEach(filename => {
+    const baseName = stripExtension(filename);
+    // Create a unique variable name for each import
+    const importVar = `${moduleName}_${baseName}`.replace(/[^a-zA-Z0-9_]/g, '_');
+    importLines.push(`import ${importVar} from './apps/${moduleName}/${filename}';`);
+    if (!moduleImports[moduleName]) {
+      moduleImports[moduleName] = {};
+    }
+    moduleImports[moduleName][baseName] = importVar;
+  });
+});
+
+// --------------------------
+// 3. PREPARE DATA FOR DYNAMIC TITLE (Optional)
+// --------------------------
+const titleEntries = [];
+
+// For top-level apps
+topLevelApps.forEach(filename => {
+  titleEntries.push(`'/${toRouteName(filename)}': '${toNiceTitle(filename)}'`);
+});
+
+// For modules themselves => raw folder name
+Object.keys(modules).forEach(moduleName => {
+  titleEntries.push(`'/${toRouteName(moduleName)}': '${moduleName}'`);
+
+  // For each app inside the module => nice title
+  modules[moduleName].forEach(filename => {
+    titleEntries.push(
+      `'/${toRouteName(moduleName)}/${toRouteName(filename)}': '${toNiceTitle(filename)}'`
+    );
+  });
+});
+
 const dynamicTitleComponent = `
 function DynamicTitle() {
   const location = useLocation();
 
   useEffect(() => {
     const titles = {
-      ${titleMap}
+      ${titleEntries.join(',\n      ')}
     };
     document.title = titles[location.pathname] || 'Interactive Learning Apps';
   }, [location]);
@@ -54,12 +130,149 @@ function DynamicTitle() {
 }
 `;
 
-// Template for the App.js content
-const appJsContent = `
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+// --------------------------
+// 4. GENERATE ROUTE ELEMENTS
+// --------------------------
 
-${imports}
+// Top-level app routes
+const routesForTopLevel = topLevelApps.map(filename => {
+  const baseName = stripExtension(filename);
+  return `
+  <Route
+    path="/${toRouteName(filename)}"
+    element={
+      <ResizeWrapper>
+        <${baseName} />
+      </ResizeWrapper>
+    }
+  />`;
+}).join('');
+
+// Module routes
+const routesForModules = [];
+Object.keys(modules).forEach(moduleName => {
+  // Module's "index" page
+  routesForModules.push(`
+    <Route
+      path="/${toRouteName(moduleName)}"
+      element={
+        <div style={{ textAlign: 'center', padding: '2rem' }}>
+          <h2>${moduleName} module</h2>
+          <ul style={{ listStyle: 'none', padding: 0 }}>
+            ${
+              modules[moduleName].map(appFile => {
+                const route = `/${toRouteName(moduleName)}/${toRouteName(appFile)}`;
+                return `
+                  <li style={{ marginBottom: '1rem' }}>
+                    <Link to="${route}" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+                      ${toNiceTitle(appFile)}
+                    </Link>
+                  </li>
+                `;
+              }).join('')
+            }
+          </ul>
+        </div>
+      }
+    />
+  `);
+
+  // A route for each app within the module
+  modules[moduleName].forEach(filename => {
+    const baseName = stripExtension(filename);
+    const importVar = moduleImports[moduleName][baseName];
+    routesForModules.push(`
+      <Route
+        path="/${toRouteName(moduleName)}/${toRouteName(filename)}"
+        element={
+          <ResizeWrapper>
+            <${importVar} />
+          </ResizeWrapper>
+        }
+      />
+    `);
+  });
+});
+
+// --------------------------
+// 5. HOMEPAGE LINKS
+// --------------------------
+const moduleLinks = Object.keys(modules).map(moduleName => {
+  const route = `/${toRouteName(moduleName)}`;
+  return `
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="${route}" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        ${moduleName}
+      </Link>
+    </li>
+  `;
+}).join('');
+
+const topLevelLinks = topLevelApps.map(filename => {
+  const route = `/${toRouteName(filename)}`;
+  return `
+    <li style={{ marginBottom: '1rem' }}>
+      <Link to="${route}" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+        ${toNiceTitle(filename)}
+      </Link>
+    </li>
+  `;
+}).join('');
+
+/**
+ * This is the JSX that goes in the "homepage" route (path="/").
+ * Notice references to 'modules' and 'topLevelApps'. We need
+ * to inject them as constants in the final output so we can do
+ * {Object.keys(modules).length > 0 && ...} safely in runtime.
+ */
+const homepageElement = `
+<div style={{ textAlign: 'center', padding: '2rem' }}>
+  <h1>Welcome to Interactive Learning Apps</h1>
+  <nav style={{ marginTop: '2rem' }}>
+
+    {/* Only show Modules heading if there's at least one module */}
+    {Object.keys(modules).length > 0 && (
+      <>
+        <h2>Modules</h2>
+        <ul style={{ listStyle: 'none', padding: 0 }}>
+          ${moduleLinks}
+        </ul>
+      </>
+    )}
+
+    {/* Only show Other Apps heading if there's at least one top-level app */}
+    {topLevelApps.length > 0 && (
+      <>
+        <h2>Other apps</h2>
+        <ul style={{ listStyle: 'none', padding: 0 }}>
+          ${topLevelLinks}
+        </ul>
+      </>
+    )}
+
+  </nav>
+</div>
+`;
+
+// --------------------------
+// 6. INJECT modules & topLevelApps into the final code
+// so that "App.js" has them as actual variables.
+// --------------------------
+const modulesJSON = JSON.stringify(modules, null, 2);
+const topLevelAppsJSON = JSON.stringify(topLevelApps, null, 2);
+
+// --------------------------
+// 7. BUILD THE FINAL App.js CONTENT
+// --------------------------
+const appJsContent = `
+${importLines.join('\n')}
+
+/**
+ * We define these constants so the JSX can reference them
+ * (and won't throw "no-undef" errors).
+ */
+const modules = ${modulesJSON};
+const topLevelApps = ${topLevelAppsJSON};
 
 ${dynamicTitleComponent}
 
@@ -68,21 +281,19 @@ function App() {
     <Router>
       <DynamicTitle />
       <Routes>
+        {/* The homepage */}
         <Route
           path="/"
           element={
-            <div style={{ textAlign: 'center', padding: '2rem' }}>
-              <h1>Welcome to Interactive Learning Apps</h1>
-              <p>Select an app to get started:</p>
-              <nav style={{ marginTop: '1rem' }}>
-                <ul style={{ listStyle: 'none', padding: 0 }}>
-                  ${links}
-                </ul>
-              </nav>
-            </div>
+            ${homepageElement}
           }
         />
-        ${routes}
+
+        {/* Top-level app routes */}
+        ${routesForTopLevel}
+
+        {/* Module routes */}
+        ${routesForModules.join('\n')}
       </Routes>
     </Router>
   );
@@ -91,6 +302,8 @@ function App() {
 export default App;
 `;
 
-// Write the updated App.js content
-fs.writeFileSync(appJsPath, appJsContent);
-console.log('App.js updated with new routes, links, and dynamic titles!');
+// --------------------------
+// 8. WRITE OUT THE GENERATED App.js
+// --------------------------
+fs.writeFileSync(appJsPath, appJsContent, 'utf-8');
+console.log('Successfully updated src/App.js with dynamic module/app routing!');
